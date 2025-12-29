9280225dd6eb302d3075d5ac9819590a1431a49f
Daryna Yusikova <darynayusikova@gmail.com>
2025-12-29 00:54:56 +0000

    Fixed the bug in task 3

--- js/task-3.js
+++ js/task-3.js
@@ -2,14 +2,7 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 input.addEventListener('input', inputHandler);
 function inputHandler(event) {
-  output.textContent = event.currentTarget.value;
-}
-
-input.addEventListener('change', removeSpace);
-
-function removeSpace(event) {
-  let name = output.textContent;
-  if (name[name.length - 1] === ' ') {
-    output.textContent = output.textContent.slice(0, -1);
-  }
+  const outputField = event.currentTarget.value.trim();
+  console.log(outputField);
+  output.textContent = outputField === '' ? 'Anonymous' : outputField;
 }
